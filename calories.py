26aa4885fa0f8af71b9a831e6f0041e107e9f9e8
joestonecodes <118644836+joestonecodes@users.noreply.github.com>
2024-05-27 21:16:27 +0000

    calculate cost of recipes

---
+++ calories.py
@@ -0,0 +1,9 @@
+base_daily_calories = 2000
+days = 7
+
+Weekly_calories = base_daily_calories * days
+display_message = "You have a budget of " +str(Weekly_calories) +" This week!"
+
+print (display_message)
+
+print 
